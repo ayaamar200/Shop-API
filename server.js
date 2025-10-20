bf8358c30ae93d1e0b187b459cef53e993f8df61
ayaamar2001 <129336185+ayaamar200@users.noreply.github.com>
2025-10-20 12:17:31 +0300

    .

--- server.js
+++ server.js
@@ -19,14 +19,13 @@ dbConnection();
 
 // App Configuration
 const app = express();
-app.use(cors()); // Enable CORS
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// ); // Allow all origins
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+); // Enable CORS
 
 // Middlewares
 // parse JSON Request Bodies
